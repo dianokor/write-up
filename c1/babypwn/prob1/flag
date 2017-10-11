6950b61f386ab0626beae5aa20541e3f9b9796c4
dianoMacpro <diano@m4cpr0-r3tin4.local>
2017-10-11 16:05:03 +0900

    init

--- c1/babypwn/prob1/flag
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("env addr: 0x%x \n", getenv("shell"));
-	return 0;
-}
--- c1/babypwn/prob3/findsh.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int arc, char* argc[]){
-	long sh;
-	sh = 0xf7e3eda0;
-	while(memcmp((void*)sh,"/bin/sh",8)) sh++;
-	printf("addr : 0x%x\n",sh);
-}
